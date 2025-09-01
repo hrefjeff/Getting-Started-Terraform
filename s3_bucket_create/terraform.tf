d0a16b0c953830cf609d588639ff361fc0fe812c
Jeffrey Allen <jeffwayne256@gmail.com>
2025-08-31 21:27:25 -0500

    Added backend config

--- s3_bucket_create/terraform.tf
+++ s3_bucket_create/terraform.tf
@@ -6,11 +6,20 @@ terraform {
     }
   }
 
+  # This uses partial configuration
+  # Run `terraform init -backend-config="key=black-hole.tfstate"`
   backend "s3" {
-    bucket = "black-hole20250901015630682000000001"
-    # key    = "black-hole.tfstate"
-    # Use following command to pass the key:
+
+    #########################################################
+    # How to configure S3 backend if not using external file
     # terraform init -backend-config="key=black-hole.tfstate"
-    region = "us-east-1"
+    #########################################################
+
+    # bucket = "black-hole20250901015630682000000001"
+    # key    = "black-hole.tfstate"
+    # region = "us-east-1"
+    # aws_access_key_id = "<YOUR_AWS_ACCESS_KEY_ID>"
+    # aws_secret_access_key = "<YOUR_AWS_SECRET_ACCESS_KEY>"
+
   }
 }
